75107375a41c363e358a45b729e8726541eafb06
yingyanwu <1179858356@qq.com>
2022-04-17 19:23:16 +0800

    项目提交

---
+++ app/src/main/java/com/example/background/BlurActivity.kt
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.background
+
+import android.Manifest
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.work.WorkInfo
+import com.bumptech.glide.Glide
+import com.example.background.databinding.ActivityBlurBinding
+import com.permissionx.guolindev.PermissionX
+import com.zhihu.matisse.Matisse
+import com.zhihu.matisse.MimeType
+import com.zhihu.matisse.engine.impl.GlideEngine
+import com.zhihu.matisse.internal.entity.CaptureStrategy
+
+const val REQUEST_CODE_CHOOSE = 101
+
+class BlurActivity : AppCompatActivity() {
+
+    private val viewModel: BlurViewModel by viewModels { BlurViewModelFactory(application) }
+    private lateinit var binding: ActivityBlurBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBlurBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.selectImageButton.setOnClickListener {
+            onClickSelectImage()
+        }
+        binding.goButton.setOnClickListener { viewModel.applyBlur(blurLevel) }
+
+        // Setup view output image file button
+        binding.seeFileButton.setOnClickListener {
+            viewModel.outputUri?.let { currentUri ->
+                Log.d("wyy", "currentUri:${currentUri}")
+                val actionView = Intent(Intent.ACTION_VIEW, currentUri)
+                actionView.resolveActivity(packageManager)?.run {
+                    startActivity(actionView)
+                }
+            }
+        }
+
+        // Hookup the Cancel button
+        binding.cancelButton.setOnClickListener { viewModel.cancelWork() }
+
+        viewModel.outputWorkInfos.observe(this, workInfosObserver())
+        viewModel.mImageUri.observe(this) {
+            Log.d("wyy", "Uri改变啦：$it")
+            Glide.with(this).load(it).into(binding.imageView)
+        }
+    }
+
+    private fun onClickSelectImage() {
+        PermissionX.init(this).permissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+            .request { allGranted, _, _ ->
+                if (allGranted) {
+                    selectImage()
+                }
+            }
+    }
+
+    private fun selectImage() {
+        Matisse.from(this).choose(MimeType.ofImage()).capture(true).captureStrategy(
+            CaptureStrategy(true, "com.example.background.fileprovider", "test")
+        ).showSingleMediaType(true).countable(true).maxSelectable(1).imageEngine(GlideEngine())
+            .forResult(REQUEST_CODE_CHOOSE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_CHOOSE && resultCode == RESULT_OK) {
+            val mSelected = Matisse.obtainResult(data)
+            viewModel.setImageUri(mSelected[0])
+            Log.d("wyy", "mSelected:$mSelected")
+        }
+    }
+
+    private fun workInfosObserver(): Observer<List<WorkInfo>> {
+        return Observer { listOfWorkInfo ->
+
+            Log.d("wyy", "listOfWorkInfo改变了")
+            // Note that these next few lines grab a single WorkInfo if it exists
+            // This code could be in a Transformation in the ViewModel; they are included here
+            // so that the entire process of displaying a WorkInfo is in one location.
+
+            // If there are no matching work info, do nothing
+            if (listOfWorkInfo.isNullOrEmpty()) {
+                return@Observer
+            }
+
+            // We only care about the one output status.
+            // Every continuation has only one worker tagged TAG_OUTPUT
+            val workInfo = listOfWorkInfo[0]
+
+            Log.d("wyy", "workInfo:$workInfo")
+            Log.d("wyy", "workInfo.state.isFinished:${workInfo.state.isFinished}")
+            if (workInfo.state.isFinished) {
+                showWorkFinished()
+
+                // Normally this processing, which is not directly related to drawing views on
+                // screen would be in the ViewModel. For simplicity we are keeping it here.
+                val outputImageUri = workInfo.outputData.getString(KEY_IMAGE_URI)
+
+                Log.d("wyy", "outputImageUri:$outputImageUri")
+                // If there is an output file show "See File" button
+                if (!outputImageUri.isNullOrEmpty()) {
+                    viewModel.setOutputUri(outputImageUri)
+                    binding.seeFileButton.visibility = View.VISIBLE
+                }
+            } else {
+                showWorkInProgress()
+            }
+        }
+    }
+
+    /**
+     * Shows and hides views for when the Activity is processing an image
+     */
+    private fun showWorkInProgress() {
+        with(binding) {
+            progressBar.visibility = View.VISIBLE
+            cancelButton.visibility = View.VISIBLE
+            goButton.visibility = View.GONE
+            seeFileButton.visibility = View.GONE
+        }
+    }
+
+    /**
+     * Shows and hides views for when the Activity is done processing an image
+     */
+    private fun showWorkFinished() {
+        with(binding) {
+            progressBar.visibility = View.GONE
+            cancelButton.visibility = View.GONE
+            goButton.visibility = View.VISIBLE
+        }
+    }
+
+    private val blurLevel: Int
+        get() =
+            when (binding.radioBlurGroup.checkedRadioButtonId) {
+                R.id.radio_blur_lv_1 -> 1
+                R.id.radio_blur_lv_2 -> 2
+                R.id.radio_blur_lv_3 -> 3
+                else -> 1
+            }
+}
